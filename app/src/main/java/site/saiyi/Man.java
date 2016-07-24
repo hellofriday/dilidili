e02e4b74605fe8050367cada5edc53295d6cd65f
丁思议 <1203159664@qq.com>
2016-07-24 23:02:57 +0800

    提交实体类

--- app/src/main/java/site/saiyi/Man.java
+++ app/src/main/java/site/saiyi/Man.java
@@ -4,6 +4,15 @@ package site.saiyi;
  * Created by 丁思议 on 2016/7/24.
  */
 public class Man {
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    private String Name;
     public Man(){
 
     }
